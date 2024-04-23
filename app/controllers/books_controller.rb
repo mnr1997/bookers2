d1cad66d3328d42d023bcbcddccec192fc493dc0
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-04-23 09:35:45 +0000

    アクセス制限

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,18 +3,25 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @user = User.find(current_user.id)
+      render :index
+    end
   end
   
   def index
     @book = Book.new
     @books = Book.all
+    @user = User.find(current_user.id)
   end
-
+  
   def show
     @book = Book.new
     @books = Book.find(params[:id])
+    @user = @books.user
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def index
     @users = User.all
     @user = User.find(current_user.id)
+    @book = Book.new
   end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
@@ -24,4 +28,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+  
 end
