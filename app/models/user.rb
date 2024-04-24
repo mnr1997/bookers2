bc62f9b5f741dc37eee42ce256d165d862a88c29
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-04-24 16:15:31 +0000

    validation途中

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+  validates :name, uniqueness: true
+  validates :name, length: { minimum: 2, maximum: 20 }
+  validates :introduction, length: { maximum: 50 }
+  
   has_many :books, dependent: :destroy
   
   has_one_attached :profile_image
