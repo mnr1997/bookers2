751fa1d5cd3cdb09cc5c16081c691aea071aaaee
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-04-28 18:04:53 +0000

    4/28

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
   
   def create
     @book = Book.new(book_params)
@@ -30,10 +31,12 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "You have updated book successfully."
       redirect_to book_path(book.id)
+    else
+      render :edit
     end
   end
   
@@ -48,4 +51,12 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+  
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+  
 end
